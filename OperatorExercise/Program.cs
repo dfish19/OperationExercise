b1b25cc2d2fdb6d68aa1dc198b25c4ae5772ba58
dfish19 <dfish19@yahoo.com>
2022-08-04 16:04:24 -0500

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            Console.WriteLine(a + " / " + b + " is " + quotient + " remainer " + remainder);
+        }
+        
+           
+    }
+}

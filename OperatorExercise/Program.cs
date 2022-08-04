2bcb33ca8f01981b1bd0baee14dab154b15a4a4e
dfish19 <dfish19@yahoo.com>
2022-08-04 16:49:12 -0500

    two on one

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -14,10 +14,20 @@ namespace OperatorExercise
             var b = 4;
             var quotient = a / b;
             var remainder = a % b;
-            Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);
+            
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine("What is the radius of your circle?");
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+
+            AreaOfCircle(radius);
+            Console.WriteLine(AreaOfCircle(radius));
         }
-        
-           
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+
     }
 }

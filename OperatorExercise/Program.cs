3c6c5ec4125fa487ea2575d3a3924de9888b3606
dfish19 <dfish19@yahoo.com>
2022-08-09 17:34:41 -0500

    trial

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -10,24 +10,52 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
-            var a = 17;
-            var b = 4;
+            int a = 17;
+            int b = 4;
             var quotient = a / b;
             var remainder = a % b;
-            
+
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine();
+
+
+
             Console.WriteLine("What is the radius of your circle?");
             var userInput = Console.ReadLine();
             var radius = double.Parse(userInput);
 
+
+
             AreaOfCircle(radius);
             Console.WriteLine(AreaOfCircle(radius));
+        }
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Modulus(int a, int b)
+        {
+            return a % b;
         }
         public static double AreaOfCircle(double radius)
         {
             var area = Math.PI * Math.Pow(radius, 2);
             return area;
         }
+       
+        }
+    }   
 
-    }
-}
+
